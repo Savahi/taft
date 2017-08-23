56886c489ff7315ef57a31ccafff6d58d090322d
Savahi <hirshhorn@yahoo.com>
2017-08-23 17:06:14 +0300

    1st commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='taft',
+      version='0.1',
+      description='Technical Analysis For Trading',
+      url='http://github.com/Savahi/taft',
+      author='Savahi',
+      author_email='Savahi@yandex.ru',
+      license='MIT',
+      packages=['taft'],
+      zip_safe=False)
